fa68f0d3b30888045bd182e79be16a273c2f6d3c
Edward Tsai <edward.tsai.0091+github@gmail.com>
2023-08-26 01:45:36 -0700

    feat: serve the next app through nest

--- workspaces/server/src/app.module.ts
+++ workspaces/server/src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@server/app.controller';
+import { AppService } from '@server/app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../client/.next/server/app'),
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
