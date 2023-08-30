a404605621884a6c1b6758a76fdc1e16a149f76e
Edward Tsai <edward.tsai.0091+github@gmail.com>
2023-08-29 23:14:51 -0700

    refactor: remove backup and deleted root route to for serving client app

--- workspaces/server/src/app.module.ts
+++ workspaces/server/src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../client/.next/server/app'),
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '../../client/.next/server/app'),
+    // }),
     GameModule,
   ],
   controllers: [],
