92cb3e451d781c4d07d6bb89e459814787a9ded9
Edward Tsai <edward.tsai.0091+github@gmail.com>
2023-09-01 13:25:52 -0700

    refactor: remove root serving for server

--- workspaces/server/src/app.module.ts
+++ workspaces/server/src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { GameModule } from './game/game.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../../../client/.next/server/app'),
-    }),
     GameModule,
   ],
   controllers: [],
