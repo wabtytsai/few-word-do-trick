0c9b4a5515f2c522c92a6cde45b2dbb5fa24082d
Edward Tsai <edward.tsai.0091+github@gmail.com>
2023-09-10 16:13:54 -0700

    update server url and remove ds store

--- workspaces/client/app/contexts/socket-context.tsx
+++ workspaces/client/app/contexts/socket-context.tsx
@@ -2,7 +2,6 @@ import { createContext, ReactNode, useContext } from "react";
 import io, { Socket } from "socket.io-client";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL as string;
-console.log(SOCKET_URL);
 const ws = io(SOCKET_URL);
 
 const SocketContext = createContext(ws);
